19c840b270bab3994f73a335ab731aec7d5fe6ed
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2021-03-27 20:57:31 -0300

    feat: add modal logic to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import Modal from "react-modal";
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  const handleNewTransactionModal = () => {
+    setIsNewTransactionModalOpen((prevState) => !prevState);
+  };
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
